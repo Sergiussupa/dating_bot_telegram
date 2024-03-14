3156600bcfaf99c44628e01385574056568a02b6
sergiussupa <snastasenko99@gmail.com>
2024-03-14 16:45:23 +0300

    ,,,,

--- src/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	telegramToken: '6778835904:AAFIqVM6b4pK15bM-cSKVaL5WbzxR4qrtOs',
-	db: {
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'telegram'
-	}
-}
